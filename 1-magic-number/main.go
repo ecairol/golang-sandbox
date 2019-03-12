a5d6d7bc7f966e9c5253fc6fa2bf032a6f2a099d
Esteban Cairol <ecairol@gmail.com>
2019-03-11 21:30:03 -0600

    Ex1. Read name from terminal and print random number.

---
+++ 1-magic-number/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt" 
+	"bufio" 
+	"os"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your name:")
+	
+	// The blank identifier _ is used whenever syntax requires a variable name but program logic does not
+	// ReadString() reads until the first occurrence of \n
+	var name, err = reader.ReadString('\n') 
+	if err != nil {
+		fmt.Println("Error." + err.Error())	
+	}
+
+	// Remove the last \n character from the string
+	// Split the array from 0 to length-1
+	if lastValidChar := len(name)-1; lastValidChar >= 0 {
+		name = name[:lastValidChar]
+	}
+
+	// Initialize global Source 
+	// Using Seed() function to initialize the default Source, since different behavior is required for each run.
+	rand.Seed(time.Now().UnixNano())
+	magicNumber := rand.Intn(99)
+	fmt.Printf("%s, you should buy the lottery ticket %d \n", name, magicNumber)
+}
